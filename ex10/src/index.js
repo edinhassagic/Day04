db5dcef082c19546e4327b7fa2da539a03028401
edin hasagic <edinhassagic@gmail.com>
2021-03-11 15:16:07 +0100

    string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,23 @@
+function main(){
+
+var myFirstName = "Edin";
+var myLastName = "Hasagic";
+var myHomeTown = "Kakanj";
+
+
+
+
+
+return{
+
+myFirstName,
+myLastName,
+myHomeTown
+
+};
+
+}
+
+
+console.log(main());
+module.exports = main;
